51918b227c44243fd8373f17147eca753ad98674
Milosav Zikic <misazikic@yahoo.de>
2016-01-24 01:25:00 +0100

    getting project to compile

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -6,8 +6,12 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main extends Application {
+	private static final Logger logger = LogManager.getLogger(Main.class);
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -23,5 +27,6 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+		logger.info("HelloWorld");
 	}
 }

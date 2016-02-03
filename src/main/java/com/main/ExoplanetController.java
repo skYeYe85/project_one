bfff9ddac98433258be60ae6b65d1df556e4ecbf
Milosav Zikic <misazikic@yahoo.de>
2016-02-03 02:20:44 +0100

    update classpath - tests were not working

--- src/main/java/com/main/ExoplanetController.java
+++ src/main/java/com/main/ExoplanetController.java
@@ -2,7 +2,6 @@ package com.main;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
--- src/main/testing/com/testSuite/TestSuite.java
+++ src/main/testing/com/testSuite/TestSuite.java
@@ -1,16 +1,16 @@
 package com.testSuite;
 
-//import org.junit.runner.RunWith;
-//import org.junit.runners.Suite;
-//
-//import com.daoTest.ExoplanetDaoTest;
-//import com.serviceTest.ExoplanetServiceTest;
-//
-//@RunWith(Suite.class)
-//@Suite.SuiteClasses({
-//	ExoplanetDaoTest.class,
-//	ExoplanetServiceTest.class
-//})
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.daoTest.ExoplanetDaoTest;
+import com.serviceTest.ExoplanetServiceTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	ExoplanetDaoTest.class,
+	ExoplanetServiceTest.class
+})
 public class TestSuite{
 	
 }
